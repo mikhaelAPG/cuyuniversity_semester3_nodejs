06ee35af9d01d1949b570a3b030d53b9aa379d3c
MikhaelAdrielPratamaGana <mikhael.adriel.pratama.gana@gmail.com>
2024-05-09 12:52:30 +0700

    node js mysql crud backend API

--- response.js
+++ response.js
@@ -1,16 +1,15 @@
 const response = (statusCode, data, message, res) => {
-  res.status(statusCode).json({
-    payload: {
-      status_code: statusCode,
-      datas: data,
+  res.send(statusCode, [
+    {
+      payload: data,
+      message,
+      metadata: {
+        prev: "",
+        next: "",
+        current: "",
+      },
     },
-    message: message,
-    pagination: {
-      prev: "",
-      next: "",
-      max: "",
-    },
-  });
+  ]);
 };
 
 module.exports = response;
